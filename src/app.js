73d152f606663be45b719d5d800a1fc256cf0bb3
Woosik Kim <iam.woosik.kim@gmail.com>
2021-05-22 22:22:03 +0900

    cors 패키지 추가

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { router } from "./rest/routes.js";
+import cors from "cors";
 
 export function createApp() {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
   app.use(router);
 
   return app;
